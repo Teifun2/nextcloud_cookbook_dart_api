c9a65edc8ceb499825baf66a85117a560c96dbb1
Teifun2 <Teifun2@users.noreply.github.com>
2023-04-09 17:44:11 +0200

    Support for more precise api version check
    This is needed to support better error messages. (If the App is upgraded but nextcloud instance app is not updated we can specify this with "is below supported version)

--- lib/src/version_supported.dart
+++ lib/src/version_supported.dart
@@ -6,15 +6,26 @@ const _supportedMinor = 1;
 
 extension CookbookVersionSupported on NcCookbookApi {
   /// Checks if the app on the server is supported by the client
-  Future<bool> isSupported() async {
+  Future<int> isSupported() async {
     final response = await getMiscApi().version();
 
     return isSupportedSync(response.data!.apiVersion);
   }
 
-  bool isSupportedSync(APIVersion apiVersion) {
-    return apiVersion.epoch == _supportedEpoch &&
+  int isSupportedSync(APIVersion apiVersion) {
+    if (apiVersion.epoch == _supportedEpoch &&
         apiVersion.major == _supportedMajor &&
-        apiVersion.minor >= _supportedMinor;
+        apiVersion.minor == _supportedMinor) {
+      // If is tested and target version
+      return 0;
+    } else if ((apiVersion.epoch >= _supportedEpoch) &&
+        (apiVersion.major >= _supportedMajor || (apiVersion.major < _supportedMajor && apiVersion.epoch > _supportedEpoch)) &&
+        (apiVersion.minor >= _supportedMinor || (apiVersion.minor < _supportedMinor && apiVersion.epoch > _supportedEpoch)) || (apiVersion.minor < _supportedMinor && apiVersion.major > _supportedMajor)) {
+      // If is above supported version
+      return 1;
+    } else {
+      // If is below supported version
+      return -1;
+    }
   }
 }
